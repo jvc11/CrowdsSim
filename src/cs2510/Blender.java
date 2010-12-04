cb9f7ff49c26a69f40c0c4d8809cd0f72a5f507c
Yann Le Gall <ylegall@gmail.com>
2010-12-03 22:18:43 -0500

    added config file and code to parse it

--- src/cs2510/Blender.java
+++ src/cs2510/Blender.java
@@ -1,13 +1,50 @@
 
 package cs2510;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  */
 public class Blender {
 
+	private Random random;
 	private Map<Integer, List<Integer>> pathMap;
+
+	public Blender() {
+		pathMap = new HashMap<Integer, List<Integer>>();
+		random = Main.random;
+	}
+
+	/**
+	 * 
+	 */
+	void shufflePaths() {
+		for (int i=0; i < Main.numJondos; i++) {
+			pathMap.put(i, getRandomPath(i));
+		}
+//		System.out.println(pathMap);	//DEBUG
+	}
+
+	/**
+	 *
+	 * @param startID
+	 * @return
+	 */
+	private List<Integer> getRandomPath(int startID) {
+		double prob;
+		int next = startID;
+		List<Integer> path = new LinkedList<Integer>();
+
+		do {
+			path.add(next);
+			prob = random.nextDouble();
+			next = random.nextInt(Main.numJondos);
+		} while (prob < Main.probForward);
+		return path;
+	}
 	
 }
--- src/cs2510/Main.java
+++ src/cs2510/Main.java
@@ -1,33 +1,108 @@
 
 package cs2510;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Properties;
 import java.util.Random;
 
 /**
  */
 public class Main {
 
-	public static double pf;
-	public static int crowdSize;
-	public static int numAttackers;
-	
-	private static Random random;
+	static double probForward;
+	static double probRequest;
+	static int numJondos;
+	static int numAttackers;
+	static int numServers;
+	static int totalDuration;
+	private static int clock;
+
+	private static int SEED;
+	static Random random;
 	
 	private static Server[] servers;
 	private static Jondo[] jondos;
-//	private static Jondo[] badJondos;
 	private static Blender blender;
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		// read configuration parameters
+		loadConfiguration("config.properties");
 
-		// run simulation
+		runSimulation();
 
-		// write results
-
+		writeResults();
 	}
+
+	/**
+	 * 
+	 */
+	private static void runSimulation() {
+
+		clock = 0;
+		random = new Random(SEED);
+		blender = new Blender();
+		servers = new Server[numServers];
+		jondos = new Jondo[numJondos];
+
+		// initialize paths
+		blender.shufflePaths();
+
+		// main loop
+		while (clock < totalDuration) {
+
+			// simulate requests
+
+			// simulate forwarding
+
+			// simulate server responding
+			
+			clock++;
+		}
+	}
+
+	/**
+	 * 
+	 * @param filename
+	 * @throws IOException
+	 */
+	private static void loadConfiguration(String filename) throws IOException {
+		Properties p = new Properties();
+		p.load(new FileReader(filename));
+
+		numJondos = Integer.parseInt(p.getProperty("numJondos"));
+		numAttackers = Integer.parseInt(p.getProperty("numAttackers"));
+		numServers = Integer.parseInt(p.getProperty("numServers"));
+		totalDuration = Integer.parseInt(p.getProperty("totalDuration"));
+		SEED = Integer.parseInt(p.getProperty("rand.seed"));
+		probForward = Double.parseDouble(p.getProperty("probForward"));
+		probRequest = Double.parseDouble(p.getProperty("probRequest"));
+	}
+
+	/**
+	 *
+	 */
+	private static void writeResults() {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new FileWriter("output.txt"));
+
+			out.println("== CrowdSim Statistics ==");
+			out.println(Calendar.getInstance().getTime());
+			out.println();
+
+		} catch (IOException ioe) {
+			System.err.println(ioe.getMessage());
+		} finally {
+			out.close();
+		}
+		
+	}
+
 }
